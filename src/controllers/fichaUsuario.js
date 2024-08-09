afb67272ef61ff4e111c66ca83cee09c28bf41a9
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-09 12:17:02 -0400

    add route contact information

--- src/controllers/fichaUsuario.js
+++ src/controllers/fichaUsuario.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const datosFamiliares = require('../services/fichaUsuario/datosFamiliares');
+const datosContactos = require('../services/fichaUsuario/datosContactos');
 
 // Rutas para datos familiares
 router.post('/datos_familiares/:email', datosFamiliares.createDatosFamiliares);
 router.patch('/datos_familiares/:email/:id', datosFamiliares.updateDatosFamiliares);
 router.get('/datos_familiares/:email', datosFamiliares.listDatosFamiliares);
 router.get('/datos_familiares/:email/:id', datosFamiliares.getDatoFamiliar);
+
+// Rutas para datos contactos
+router.post('/datos_contactos/:email', datosContactos.createDatosContactos);
+router.patch('/datos_contactos/:email/:id', datosContactos.updateDatosContactos);
+router.get('/datos_contactos/:email', datosContactos.listDatosContactos);
+router.get('/datos_contactos/:email/:id', datosContactos.getDatoContacto);
 
 
 module.exports = router;
