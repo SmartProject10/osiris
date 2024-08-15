96f25b40261cb2093ddffc79b979ffe2931284b7
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-15 16:50:25 -0400

    refactor DatosContactos

--- src/model/fichaUsuario/datosContactos.js
+++ src/model/fichaUsuario/datosContactos.js
@@ -1,34 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
 
-const sedeSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const sedeSchema = new mongoose.Schema({
+  fichaUsuarioId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "FichaUsuarios",
+    required: true,
   },
   direccionActualizada: {
     type: String,
-    trim: true
+    trim: true,
   },
   personaContacto: {
     type: String,
-    trim: true
+    trim: true,
   },
   ocupacionLugarTrabajo: {
     type: String,
-    trim: true
+    trim: true,
   },
   celular: {
     type: String,
     trim: true,
-    validate: {
-      validator: function(value) {
-        return /\d{10}/.test(value); // Valida que el número de celular tenga 10 dígitos
-      },
-      message: 'El número de celular debe tener 10 dígitos'
-    }
-  }
+  },
 });
 
-module.exports = mongoose.model('DatosContactos', sedeSchema);
+module.exports = mongoose.model("DatosContactos", sedeSchema);
