ff0f7977a07cfb7012ecd26b9781c8f30c6790eb
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-15 16:53:21 -0400

    refactor DatosContactos services

--- src/services/fichaUsuario/datosContactos.js
+++ src/services/fichaUsuario/datosContactos.js
@@ -1,54 +1,47 @@
-const DatosContactos = require('../../model/fichaUsuario/datosContactos');
-const { connectToMongoClient } = require('../../config/db');
+const DatosContactos = require("../../model/fichaUsuario/datosContactos");
+const FichaUsuarios = require("../../model/fichaUsuario/fichaUsuarioSchema");
 
 const createDatosContactos = async (req, res) => {
-  const { email } = req.params;
+  const { fichaUsuarioId } = req.params;
   const data = req.body;
   try {
-    const client = await connectToMongoClient();
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
+    if (fichaUsuario) {
       // Validar datos
       if (!data || Object.keys(data).length === 0) {
-        return res.status(400).send('Datos de contacto no proporcionados');
+        return res.status(400).send("Datos de contacto no proporcionados");
       }
-      // Asignar el userId al nuevo objeto de datos de contacto
-      data.userId = usuario._id;
+      // Asignar el fichaUsuarioId al nuevo objeto de datos de contacto
+      data.fichaUsuarioId = fichaUsuario._id;
 
-      // Crear una nueva ficha de usuario con los datos adicionales
-      const nuevaFichaUsuario = new DatosContactos(data);
+      // Crear una nueva entrada de datos de contacto
+      const nuevosDatosContactos = new DatosContactos(data);
 
-      // Guardar la nueva ficha de usuario
-      await nuevaFichaUsuario.save();
-      res.status(200).send(nuevaFichaUsuario);
+      // Guardar los nuevos datos de contacto
+      await nuevosDatosContactos.save();
+      res.status(200).send(nuevosDatosContactos);
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Ficha de usuario no encontrada");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const updateDatosContactos = async (req, res) => {
-  const { email, id } = req.params;
+  const { fichaUsuarioId, id } = req.params;
   const data = req.body;
 
   try {
-    const client = await connectToMongoClient();
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
+    if (fichaUsuario) {
       // Buscar los datos de contacto existentes por userId y id
-      const datosContactos = await DatosContactos.findOne({
-        userId: usuario._id,
-        _id: id,
-      });
+      const datosContactos = await DatosContactos.findById(id);
 
       if (datosContactos) {
         // Actualizar los datos de contacto con la nueva información
@@ -58,41 +51,45 @@ const updateDatosContactos = async (req, res) => {
         await datosContactos.save();
         res.status(200).send(datosContactos);
       } else {
-        res.status(404).send('Datos de contacto no encontrados');
+        res.status(404).send("Datos de contacto no encontrados");
       }
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Usuario no encontrado");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const listDatosContactos = async (req, res) => {
-  const { email } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  const { fichaUsuarioId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = "direccionActualizada",
+    order = "asc",
+  } = req.query;
 
   try {
-    const client = await connectToMongoClient();
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
+    if (fichaUsuario) {
       // Calcular el número de documentos a saltar
       const skip = (page - 1) * limit;
 
-      // Obtener los datos de contacto con paginación
+      // Obtener los datos de contacto con paginación y ordenación
       const datosContactos = await DatosContactos.find({
-        userId: usuario._id,
+        fichaUsuarioId: fichaUsuario._id,
       })
+        .sort({ [sortBy]: order === "asc" ? 1 : -1 })
         .skip(skip)
         .limit(parseInt(limit));
 
       // Contar el total de documentos
       const total = await DatosContactos.countDocuments({
-        userId: usuario._id,
+        fichaUsuarioId: fichaUsuario._id,
       });
 
       res.status(200).send({
@@ -102,41 +99,36 @@ const listDatosContactos = async (req, res) => {
         data: datosContactos,
       });
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Ficha de usuario no encontrada");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const getDatoContacto = async (req, res) => {
-  const { email, id } = req.params;
+  const { fichaUsuarioId, id } = req.params;
 
   try {
-    const client = await connectToMongoClient();
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
-      // Buscar el dato de contacto específico por userId y el id del dato de contacto
-      const datoContacto = await DatosContactos.findOne({
-        userId: usuario._id,
-        _id: id,
-      });
+    if (fichaUsuario) {
+      // Buscar los datos de contacto existentes por userId y id
+      const datoContacto = await DatosContactos.findById(id);
 
       if (datoContacto) {
         res.status(200).send(datoContacto);
       } else {
-        res.status(404).send('Dato de contacto no encontrado');
+        res.status(404).send("Dato de contacto no encontrado");
       }
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Usuario no encontrado");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
