ebce02a668db5b6a5a992414330925df32b690e5
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-15 16:52:28 -0400

    refactor DatosFamiliares services

--- src/services/fichaUsuario/datosFamiliares.js
+++ src/services/fichaUsuario/datosFamiliares.js
@@ -1,55 +1,47 @@
-const DatosFamiliares = require('../../model/fichaUsuario/datosFamiliares');
-const { connectToMongoClient } = require('../../config/db');
+const DatosFamiliares = require("../../model/fichaUsuario/datosFamiliares");
+const FichaUsuarios = require("../../model/fichaUsuario/fichaUsuarioSchema");
 
 const createDatosFamiliares = async (req, res) => {
-  const { email } = req.params;
+  const { fichaUsuarioId } = req.params;
   const data = req.body;
   try {
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    const client = await connectToMongoClient()
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
-
-    if (usuario) {
+    if (fichaUsuario) {
       // Validar datos
       if (!data || Object.keys(data).length === 0) {
-        return res.status(400).send('Datos familiares no proporcionados');
+        return res.status(400).send("Datos familiares no proporcionados");
       }
-      // Asignar el userId al nuevo objeto de datos familiares
-      data.userId = usuario._id;
+      // Asignar el fichaUsuarioId al nuevo objeto de datos familiares
+      data.fichaUsuarioId = fichaUsuario._id;
 
-      // Crear una nueva ficha de usuario con los datos adicionales
-      const nuevaFichaUsuario = new DatosFamiliares(data);
+      // Crear una nueva entrada de datos familiares
+      const nuevosDatosFamiliares = new DatosFamiliares(data);
 
-      // Guardar la nueva ficha de usuario
-      await nuevaFichaUsuario.save();
-      res.status(200).send(nuevaFichaUsuario);
+      // Guardar los nuevos datos familiares
+      await nuevosDatosFamiliares.save();
+      res.status(200).send(nuevosDatosFamiliares);
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Ficha de usuario no encontrada");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const updateDatosFamiliares = async (req, res) => {
-  const { email, id } = req.params;
+  const { fichaUsuarioId, id } = req.params;
   const data = req.body;
 
   try {
-    const client = await connectToMongoClient()
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
+    if (fichaUsuario) {
       // Buscar los datos familiares existentes por userId y id
-      const datosFamiliares = await DatosFamiliares.findOne({
-        userId: usuario._id,
-        _id: id,
-      });
+      const datosFamiliares = await DatosFamiliares.findById(id);
 
       if (datosFamiliares) {
         // Actualizar los datos familiares con la nueva información
@@ -59,41 +51,45 @@ const updateDatosFamiliares = async (req, res) => {
         await datosFamiliares.save();
         res.status(200).send(datosFamiliares);
       } else {
-        res.status(404).send('Datos familiares no encontrados');
+        res.status(404).send("Datos familiares no encontrados");
       }
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Usuario no encontrado");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const listDatosFamiliares = async (req, res) => {
-  const { email } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  const { fichaUsuarioId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = "nombreCompleto",
+    order = "asc",
+  } = req.query;
 
   try {
-    const client = await connectToMongoClient()
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
+    if (fichaUsuario) {
       // Calcular el número de documentos a saltar
       const skip = (page - 1) * limit;
 
-      // Obtener los datos familiares con paginación
+      // Obtener los datos familiares con paginación y ordenación
       const datosFamiliares = await DatosFamiliares.find({
-        userId: usuario._id,
+        fichaUsuarioId: fichaUsuario._id,
       })
+        .sort({ [sortBy]: order === "asc" ? 1 : -1 })
         .skip(skip)
         .limit(parseInt(limit));
 
       // Contar el total de documentos
       const total = await DatosFamiliares.countDocuments({
-        userId: usuario._id,
+        fichaUsuarioId: fichaUsuario._id,
       });
 
       res.status(200).send({
@@ -103,41 +99,36 @@ const listDatosFamiliares = async (req, res) => {
         data: datosFamiliares,
       });
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Ficha de usuario no encontrada");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
 const getDatoFamiliar = async (req, res) => {
-  const { email, id } = req.params;
+  const { fichaUsuarioId, id } = req.params;
 
   try {
-    const client = await connectToMongoClient()
-    const coll = client.db('isoDb').collection('user');
-    // Buscar usuario por email
-    const usuario = await coll.findOne({ email });
+    // Buscar ficha de usuario por ID
+    const fichaUsuario = await FichaUsuarios.findById(fichaUsuarioId);
 
-    if (usuario) {
-      // Buscar el dato familiar específico por userId y el id del dato familiar
-      const datoFamiliar = await DatosFamiliares.findOne({
-        userId: usuario._id,
-        _id: id,
-      });
+    if (fichaUsuario) {
+      // Buscar los datos familiares existentes por userId y id
+      const datoFamiliar = await DatosFamiliares.findById(id);
 
       if (datoFamiliar) {
         res.status(200).send(datoFamiliar);
       } else {
-        res.status(404).send('Dato familiar no encontrado');
+        res.status(404).send("Dato familiar no encontrado");
       }
     } else {
-      res.status(404).send('Usuario no encontrado');
+      res.status(404).send("Usuario no encontrado");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error del servidor');
+    res.status(500).send("Error del servidor");
   }
 };
 
