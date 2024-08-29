1cd7f31a54ca347b32da5a0e29068e3d39c02db9
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-28 21:49:09 -0400

    create passportConfigJwt

---
+++ src/middlewares/passportConfigJwt.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const User = require('../model/userSchema');
+require('dotenv').config();
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      console.log(jwt_payload.id)
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
+
+module.exports = passport;
+
+
